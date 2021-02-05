3394c4212f6be94359ffff2afcb9b2bce593ba68
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-05 18:05:10 +0100

    wip

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/services/head.service';
 export * from './lib/templates/explorer/explorer.component';
 export * from './lib/title/title.component';
 export * from './lib/ui.module';
