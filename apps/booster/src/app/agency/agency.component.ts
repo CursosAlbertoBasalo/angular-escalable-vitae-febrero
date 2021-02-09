551c2caed6d55125768d6dfe85e2bdac1f305cf0
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-09 17:36:01 +0100

    feat: agency and launches

--- apps/booster/src/app/agency/agency.component.ts
+++ apps/booster/src/app/agency/agency.component.ts
@@ -1,25 +1,86 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { forkJoin, Observable } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { Agency } from '../core/models/Agency';
+import { Launch } from '../core/models/Launch';
 import { AgenciesService } from '../core/services/agencies.service';
+import { LaunchesService } from '../core/services/launches.service';
 @Component({
   selector: 'vitae-agency',
   templateUrl: './agency.component.html',
   styleUrls: ['./agency.component.css'],
 })
 export class AgencyComponent implements OnInit {
-  agency$: Observable<unknown>;
+  agency$: Observable<Agency>;
+  launches$: Observable<Launch[]>;
+  allResults$: Observable<unknown[]>;
 
   constructor(
     private route: ActivatedRoute,
-    private agencies: AgenciesService
+    private agencies: AgenciesService,
+    private launches: LaunchesService
   ) {}
 
   ngOnInit(): void {
+    // this.getAgencyAndThenItsLaunches();
+    // this.getAgencyAndItsLaunchesInParallelWaitingForTheLatest();
+    this.getAgencyAndItsLaunchesInParallel();
+  }
+
+  // 2.9 kB / 4.2 MB transferred
+  // Finish: 1.59 s
+  // DOMContentLoaded: 471 ms
+  // Load: 543 ms
+  private getAgencyAndThenItsLaunches() {
     this.agency$ = this.route.params.pipe(
       map((params) => params.id),
-      switchMap((agencyName) => this.agencies.getByname(agencyName))
+      switchMap((agencyName) => this.agencies.getByname(agencyName)),
+      tap(
+        (agency) =>
+          (this.launches$ = this.launches.getByQuery({
+            numberOfLaunches: 1,
+            searchTerm: agency.name,
+          }))
+      )
     );
+  }
+
+  // 2.6 kB / 4.2 MB transferred
+  // Finish: 905 ms
+  // DOMContentLoaded: 507 ms
+  // Load: 595 ms
+  private getAgencyAndItsLaunchesInParallelWaitingForTheLatest() {
+    this.allResults$ = this.route.params.pipe(
+      map((params) => params.id),
+      switchMap((agencyName) =>
+        forkJoin([
+          this.agencies.getByname(agencyName),
+          this.launches.getByQuery({
+            numberOfLaunches: 1,
+            searchTerm: agencyName,
+          }),
+        ])
+      )
+    );
+  }
+
+  // 2.8 kB / 4.2 MB transferred
+  // Finish: 1.06 s
+  // DOMContentLoaded: 431 ms
+  // Load: 492 ms
+  private getAgencyAndItsLaunchesInParallel() {
+    this.route.params
+      .pipe(
+        map((params) => params.id),
+        tap((agencyName) => {
+          this.agency$ = this.agencies.getByname(agencyName);
+          this.launches$ = this.launches.getByQuery({
+            numberOfLaunches: 1,
+            searchTerm: agencyName,
+          });
+        })
+      )
+      .subscribe();
   }
 }
